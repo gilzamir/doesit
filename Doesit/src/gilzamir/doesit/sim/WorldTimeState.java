1c2c11b8854ce4b95ad34953c971d241a9312e08
Gilzamir Gomes <gilzamir@gmail.com>
2015-10-04 09:53:36 -0300

    Bug fix.

--- Doesit/src/gilzamir/doesit/sim/WorldTimeState.java
+++ Doesit/src/gilzamir/doesit/sim/WorldTimeState.java
@@ -3,10 +3,13 @@ package gilzamir.doesit.sim;
 import com.jme3.app.Application;
 import com.jme3.app.state.AbstractAppState;
 import com.jme3.app.state.AppStateManager;
+import com.jme3.collision.CollisionResult;
+import com.jme3.collision.CollisionResults;
 import com.jme3.light.AmbientLight;
 import com.jme3.light.DirectionalLight;
 import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
+import com.jme3.math.Ray;
 import com.jme3.math.Vector3f;
 import com.jme3.post.FilterPostProcessor;
 import com.jme3.scene.Geometry;
@@ -97,14 +100,62 @@ public class WorldTimeState extends AbstractAppState {
         
 
         DirectionalLight l = sunLight;
-        Vector3f ldir = l.getDirection();
+        Vector3f ldir = l.getDirection().negate();
+        
+        float dayRate = Math.max(0, ldir.dot(Vector3f.UNIT_Y));
+        
+        
         ColorRGBA lcolor = l.getColor();
         float alfa = normal.dot(ldir);
         alfa = Math.max(0.0f, Math.min(alfa, 1.0f));
-        result.x += lcolor.getRed() * alfa * diffuse.getRed();
-        result.y += lcolor.getGreen() * alfa * diffuse.getGreen();
-        result.z += lcolor.getBlue() * alfa * diffuse.getBlue();
+        result.x += lcolor.getRed() * alfa * diffuse.getRed() * dayRate;
+        result.y += lcolor.getGreen() * alfa * diffuse.getGreen() * dayRate;
+        result.z += lcolor.getBlue() * alfa * diffuse.getBlue() * dayRate;
         
         return result;
+    }
+    
+    private Ray lightRay = new Ray();
+    private int maxReflections = 3;
+    public void getReflectionColor(Material mat, Vector3f from, Vector3f normal, ColorRGBA result, int counter) {
+        lightRay.setOrigin(from);
+        Vector3f dir = sunModel.getWorldTranslation().subtract(from);
+        dir.normalize();
+        lightRay.setDirection(dir);
+        CollisionResults colresults = new CollisionResults();
+       
+        app.getSceneNode().collideWith(lightRay, colresults);
+       
+        
+        if (colresults.size() > 0) {
+            CollisionResult colresult = colresults.getClosestCollision();
+           
+            if (colresult.getGeometry() == sunModel) {
+               Vector3f c = getColorOf(mat, dir, normal, dir);
+               result.r = c.x;
+               result.g = c.y;
+               result.b = c.z;
+            } else if (counter <= maxReflections) {
+               
+                Vector3f newFrom = colresult.getContactPoint();
+                Vector3f newNormal = colresult.getContactNormal();
+                ColorRGBA blend = new ColorRGBA(0,0,0,1);
+                getReflectionColor(mat, newFrom, newNormal, blend, counter+1);
+                result.r += blend.r;
+                result.g += blend.g;
+                result.b += blend.b;
+                result.r = Math.min(1.0f, result.r);
+                result.g = Math.min(1.0f, result.g);
+                result.b = Math.min(1.0f, result.b);
+            } else { 
+                result.r = 0.0f;
+                result.b = 0.0f;
+                result.g = 0.0f;
+            }
+        } else {
+                result.r = 0.0f;
+                result.b = 0.0f;
+                result.g = 0.0f;            
+        }
     }
 }
